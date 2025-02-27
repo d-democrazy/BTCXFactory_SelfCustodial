3047d926c58aabdcefcc1b7cc63df204be6a04c6
d-democrazy <anggasudarman1993@gmail.com>
2025-02-27 17:51:09 +0700

    CollateralManager final refactor

--- script/CollateralManager/DeployCollateralManagerProxy.s.sol
+++ script/CollateralManager/DeployCollateralManagerProxy.s.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.18;
 
 import "forge-std/Script.sol";
+import "forge-std/console2.sol";
 import {CollateralManagerProxy} from "../../src/Aggregators/CollateralManager/CollateralManagerProxy.sol";
 import {CollateralManagerImplementation} from
     "../../src/Aggregators/CollateralManager/CollateralManagerImplementation.sol";
@@ -13,7 +14,7 @@ contract DeployCollateralManagerProxy is Script {
     function run() external {
         vm.startBroadcast();
 
-        // Print list of initially allowed collaterals
+        // Log allowed collateral addresses
         address[] memory allowedCollaterals = HelperConfig.getAllowedCollaterals(block.chainid);
         console.log("Deploying on network with collateral addresses:");
         for (uint256 i = 0; i < allowedCollaterals.length; i++) {
@@ -27,8 +28,8 @@ contract DeployCollateralManagerProxy is Script {
         console.log("Using implementation at:", implementationAddress);
 
         // Encode initialization data by passing allowedCollateras to initialize.
-        bytes memory initData =
-            abi.encodeWithSelector(CollateralManagerImplementation.initialize.selector, allowedCollaterals);
+        bytes memory initData = HelperConfig.encodeInitializeCall(block.chainid);
+        console2.logBytes(initData);
 
         // Deploy Proxy
         CollateralManagerProxy proxy = new CollateralManagerProxy(implementationAddress, msg.sender, initData);
--- script/CollateralManager/HelperConfig.s.sol
+++ script/CollateralManager/HelperConfig.s.sol
@@ -6,42 +6,82 @@
  */
 pragma solidity ^0.8.18;
 
+import {CollateralManagerImplementation} from
+    "../../src/Aggregators/CollateralManager/CollateralManagerImplementation.sol";
 import {Vm} from "forge-std/Vm.sol";
+import "forge-std/Script.sol";
+import "forge-std/console2.sol";
 
 library HelperConfig {
     Vm public constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
 
-    struct NetworkConfig {
-        address[] collateralAddress;
+    function getCoreTestnetConfig() internal view returns (address[] memory) {
+        string[] memory collateral = new string[](4);
+        collateral[0] = "CoreBTC";
+        collateral[1] = "SolvBTC";
+        collateral[2] = "StBTC";
+        collateral[3] = "WBTC";
+
+        return getAddresses(collateral);
     }
 
-    function getCoreTestnetConfig() internal view returns (NetworkConfig memory) {
-        address[] memory collateralAddress = new address[](2);
-        collateralAddress[0] = vm.envAddress("WCORE");
-        collateralAddress[1] = vm.envAddress("stCORE");
+    function getCoreTestnet2Config() internal view returns (address[] memory) {
+        string[] memory collateral = new string[](2);
+        collateral[0] = "WCORE";
+        collateral[1] = "stCORE";
 
-        return NetworkConfig(collateralAddress);
+        return getAddresses(collateral);
     }
 
-    function getCoreMainnetConfig() internal view returns (NetworkConfig memory) {
-        address[] memory collateralAddress = new address[](6);
-        collateralAddress[0] = vm.envAddress("solvBTCb");
-        collateralAddress[1] = vm.envAddress("solvBTCm");
-        collateralAddress[2] = vm.envAddress("oBTC");
-        collateralAddress[3] = vm.envAddress("aBTC");
-        collateralAddress[4] = vm.envAddress("suBTC");
-        collateralAddress[5] = vm.envAddress("stBTC");
+    function getCoreMainnetConfig() internal view returns (address[] memory) {
+        string[] memory collateral = new string[](6);
+        collateral[0] = "solvBTCb";
+        collateral[1] = "solvBTCm";
+        collateral[2] = "oBTC";
+        collateral[4] = "aBTC";
+        collateral[5] = "stBTC";
 
-        return NetworkConfig(collateralAddress);
+        return getAddresses(collateral);
+    }
+
+    function getAddresses(string[] memory collateral) internal view returns (address[] memory addrs) {
+        addrs = new address[](collateral.length);
+        for (uint256 i = 0; i < collateral.length; i++) {
+            addrs[i] = vm.envAddress(collateral[i]);
+        }
     }
 
     function getAllowedCollaterals(uint256 chainId) internal view returns (address[] memory) {
-        if (chainId == vm.envUint("CORE_TESTNET_CHAINID")) {
-            return getCoreTestnetConfig().collateralAddress;
-        } else if (chainId == vm.envUint("CORE_MAINNET_CHAINID")) {
-            return getCoreMainnetConfig().collateralAddress;
+        uint256 coreTestnet2ChainId = vm.envUint("CORE_TESTNET2_CHAINID");
+        uint256 coreTestnetChainId = vm.envUint("CORE_TESTNET_CHAINID");
+        uint256 coreMainnetChainId = vm.envUint("CORE_MAINNET_CHAINID");
+
+        if (chainId == coreTestnet2ChainId) {
+            return getCoreTestnet2Config();
+        } else if (chainId == coreMainnetChainId) {
+            return getCoreMainnetConfig();
+        } else if (chainId == coreTestnetChainId) {
+            return getCoreTestnetConfig();
         } else {
             revert("Unsupported chain");
         }
+    }
+
+    function encodeInitializeCall(uint256 chainId) internal view returns (bytes memory) {
+        address[] memory allowed = getAllowedCollaterals(chainId);
+        return abi.encodeWithSelector(CollateralManagerImplementation.initialize.selector, allowed);
+    }
+}
+
+// Encoder tool
+contract EncodeCollateralData is Script {
+    function run() external view {
+        address[] memory collaterals = HelperConfig.getAllowedCollaterals(block.chainid);
+
+        // Encode the function call using abi.encodeWithSignature.
+        bytes memory collateralData = abi.encodeWithSignature("initialize(address[])", collaterals);
+
+        // Log the encoded data.
+        console2.logBytes(collateralData);
     }
 }
